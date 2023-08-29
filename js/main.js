eb3931aabf9cda771c9e66b4e026d69d0156f4eb
Ismoil <ismoilanvarov26@gmail.com>
2023-08-29 19:04:01 +0500

    fix: fixed mobile menu (29.08.2023/19:04)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 const burger = document.querySelector('.burger .fa-bars-staggered')
 const close = document.querySelector('.burger .fa-times')
 const mobile__menu = document.querySelector('.header .mobile__menu')
-let winWidth = window.innerWidth
 
 burger.addEventListener('click', () => {
   mobile__menu.style.transform = 'translateX(0)'
@@ -13,4 +12,13 @@ close.addEventListener('click', () => {
   mobile__menu.style.transform = 'translateX(-100%)'
   close.style.display = 'none'
   burger.style.display = 'block'
+})
+
+window.addEventListener('resize', () => {
+  let winWidth = window.innerWidth
+  if(winWidth > 900) {
+    mobile__menu.style.transform = 'translateX(-100%)'
+    close.style.display = 'none'
+    burger.style.display = 'block'
+  }
 })
